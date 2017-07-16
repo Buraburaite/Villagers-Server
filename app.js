26b5bd4b0ac923aca7a0a8964dcbb95927eb81c1
Javier <jvrcueto@outlook.com>
2017-07-16 17:21:25 -0400

    dist

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const passport     = require('passport');
 
 dotenv.config();
 
+console.log('got to app at least, damn');
 
 // Configure and initialize mongoose middleware
 mongoose.connect(process.env.MONGODB_URI, { useMongoClient: true });
