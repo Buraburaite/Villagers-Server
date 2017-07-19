99bb2d166847596e197c62dcafbd098de559e6e6
Javier <jvrcueto@outlook.com>
2017-07-19 03:02:56 -0400

    added engines to package json

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -10,7 +10,7 @@ const authRoutes = Router();
 
 //======================================================================== LOGIN
 // Route for securely logging in a user
-authRoutes.post('/login', (req, res, next) => {
+authRoutes.post('login', (req, res, next) => {
 
   console.log('here1');
 
