1f363cfb32d83102b404dbf8f681d2f5a7ceaf8d
Javier <jvrcueto@outlook.com>
2017-07-19 03:48:21 -0400

    heroku

--- routes/index-routes.js
+++ routes/index-routes.js
@@ -1,10 +1,10 @@
 const Router = require('express').Router;
 const Post   = require('../models/post-model.js');
 
-const router = Router();
+const indexRoutes = Router();
 
 indexRoutes.get('/here', (req, res, next) => {
   console.log('here route reached');
 });
 
-module.exports = router;
+module.exports = indexRoutes;
